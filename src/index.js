610419dbc14e0e8731ddc27c83f8871d9d59fe03
Renzo dottori <141673461+Renzo005@users.noreply.github.com>
2024-09-02 21:26:21 -0300

    A chequear el POST

--- src/index.js
+++ src/index.js
@@ -11,16 +11,56 @@ app.get('/series', (req,res) => {
     res.status(200).json({series});
 })
 
+app.get('/series/:id', (req,res) => {
+    const id = req.params.id
+    const serie = series.find(serie => serie.id == id)
+    if (serie)
+        res.status(200).json({serie});
+    else
+        res.status(404).json({
+            mensaje: `El id ${id} no se encuentra.`
+        })
+})
+
+app.delete('/series/:id', (req,res) => {
+    const id = req.params.id
+    const idx = series.findIndex(serie => serie.id == id)
+
+    if (idx >= 0)
+        series.splice(idx,1),
+        res.status(200).json({
+            mensaje: `El id ${id} fue eliminado.`
+        });
+    else
+        res.status(404).json({
+            mensaje: `El id ${id} no existe.`
+        })
+})
+
 app.post('/series', (req, res) => {
     const { nombre, plataforma } = req.body;
     const serie = {
+        id: series.reduce((acum, series) => acum > series.id ? acum : series.id, 0) + 1,
         nombre,
         plataforma,
-        disponible: false
+        disponible
     };
     series.push(serie);
     res.status(201).json({ serie });
 });
+
+app.put(('/series', (req, res) => {
+    const id = req.params.id
+    const serieBody = req.body
+    const idx = series.findIndex(serie => serie.id == id)
+    if(idx >= 0){
+        const series = {id:series[idx].id, ...serie}
+        series (idx) = serie
+        res.status(201).json(serie)}
+    else
+        res.status(404).json({
+            mensaje: `El id ${id} no existe.`})
+}));
 
 app.listen(PORT, () => {
     console.log(`Aplicacion lista escuchando en ${PORT}`)
